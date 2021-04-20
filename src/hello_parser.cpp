99eaf03851b0e9e4d1a9e78d9306a48473374b53
ybzxw <541956308@qq.com>
2021-04-20 15:22:00 +0800

    增加解析器

--- src/hello_parser.cpp
+++ src/hello_parser.cpp
@@ -1,9 +1,8 @@
 #include <assert.h>
 #include <errno.h>
-#include <exception>
 #include "hello_parser.h"
 #include "hello_value.h"
-
+#include "hello_exception.h"
 
 
 
@@ -21,7 +20,26 @@ Parser::Parser(Value &val, const std::string &content)
 {
     val_.set_type(Type::TYPE_NULL);
     parser_whitespace();
+    parser_value();
     parser_whitespace();
+
+}
+
+/**
+ * @brief 解析接口
+ * 
+ */ 
+void Parser::parser_value()
+{
+    switch (*cur_)
+    {
+    case 'n': parser_literal("null", TYPE_NULL); return;
+    case 't': parser_literal("true", TYPE_TRUE); return;
+    case 'f': parser_literal("false", TYPE_FALSE); return;
+    case '\0': throw(Exception("parse expect value")); return;
+    
+    default: break;
+    }
 
 }
 
@@ -41,15 +59,12 @@ void Parser::parser_literal(const char *literal, Type type)
 {
     expect(cur_, *literal);
     size_t i = 0;
-    for(i = 0; literal[i+1]; ++i){
+    for(i = 0; literal[i+1] != '\0'; ++i){
         if(cur_[i] != literal[i+1])
-            throw (Exception)
+            throw (Exception("parse invalid value"));
     }
-    
+    cur_ += i;
+    val_.set_type(type);
 }
-
-
-
-
 
 }
